ff806315ff568202859ed859c6b3361116fdaddd
Oğuzhan <kayaoguzhan@windowslive.com>
2023-09-12 22:10:49 +0300

    practice continue

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="bg-black">
+      <div>
+        <p className="text-xs  sm:text-base md:text-lg lg:text-3xl text-blue-600 ">
+          {" "}
+          Hello asdasdasd
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      <div className="bg-red">
+        <p className="shadow-inner text-xs m-10 text-white ">anote</p>
+      </div>
     </div>
   );
 }
